f0b567e08b59f72080c81e9e450521374d281642
Foo-Maker <Foo-Maker@Sven-Bast.eu>
2021-09-14 23:17:02 +0200

    Schleife optimiert

--- kite_online/start.py
+++ kite_online/start.py
@@ -28,20 +28,26 @@ while not i:
     time_string = time.strftime("%m/%d/%Y, %H:%M:%S", named_tuple)
     uhrzeit = time_string
     r = requests.get("https://linux.kite.com")
-    if r.status_code == 404:
+    """
+    if r.status_code == 3404:
         print(f"{time_string} kommt 404")
         time.sleep(2)
         continue
-    elif r.status_code == 502:
+    elif r.status_code == 3502:
         print(f"{time_string} kommt 502")
         time.sleep(2)
         continue
-    else:
+    el
+    """
+    if r.status_code == 200:
         os.system("/opt/kite/kite-installer update")
         print(r.status_code)
         print(type(r))
         i = False
         install = True
+    else:
+        print(f"{uhrzeit} Wir haben einen {r.status_code} Fehler")
+        time.sleep(2)
 
 listener.join()
 

dfa77449085cd05019a0a8834db4d80e2d8f8dc1
Foo-Maker <Foo-Maker@Sven-Bast.eu>
2021-09-14 17:53:58 +0200

    Ist linux.kite.com online?

---
+++ kite_online/README.md
@@ -0,0 +1,10 @@
+#Is Kite online?
+
+Dies Programme ist zum schauen ob linux.kite.com erreichbar ist.
+Aktuell (14.09.2021) ist die Page offline und darum lässt sich 
+kite nich installieren.
+
+Wenn kein 404 oder 502er Fehler kommt, dann startet die Installation.
+
+So lange noch der die Fehler erschienen kann das Programm mie der 
+Eingabetaste abgebrochen werden.
---
+++ kite_online/start.py
@@ -0,0 +1,51 @@
+#!/bin/python3
+
+import requests
+import os
+from threading import Thread 
+import time 
+
+print("Starte Programm…")
+print(f"")
+print(f"Mit der Eingabetaste kann das Programm abgebrochen werden!")
+print()
+
+i = False
+install = False
+uhrzeit = ""
+
+def listen():
+    global i
+    input()
+    i = True
+
+listener = Thread(target=listen)
+listener.start()
+
+
+while not i:
+    named_tuple = time.localtime() # get struct_time
+    time_string = time.strftime("%m/%d/%Y, %H:%M:%S", named_tuple)
+    uhrzeit = time_string
+    r = requests.get("https://linux.kite.com")
+    if r.status_code == 404:
+        print(f"{time_string} kommt 404")
+        time.sleep(2)
+        continue
+    elif r.status_code == 502:
+        print(f"{time_string} kommt 502")
+        time.sleep(2)
+        continue
+    else:
+        os.system("/opt/kite/kite-installer update")
+        print(r.status_code)
+        print(type(r))
+        i = False
+        install = True
+
+listener.join()
+
+if install:
+    print(f"Kite ist um {uhrzeit} installiert worden")
+else:
+    print(f"Es wurde um {uhrzeit} abgebrochen")

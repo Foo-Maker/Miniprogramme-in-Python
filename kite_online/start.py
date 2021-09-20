0ed127c21a7d18f79b2294c1f364b85632ef7966
Foo-Maker <Foo-Maker@Sven-Bast.eu>
2021-09-20 21:53:15 +0200

    Fehler behoben der bei Fehler 101 das Programm abgebrochen hat

--- kite_online/start.py
+++ kite_online/start.py
@@ -22,23 +22,11 @@ def listen():
 listener = Thread(target=listen)
 listener.start()
 
-
-while not i:
+def chek():
     named_tuple = time.localtime() # get struct_time
     time_string = time.strftime("%m/%d/%Y, %H:%M:%S", named_tuple)
     uhrzeit = time_string
     r = requests.get("https://linux.kite.com")
-    """
-    if r.status_code == 3404:
-        print(f"{time_string} kommt 404")
-        time.sleep(2)
-        continue
-    elif r.status_code == 3502:
-        print(f"{time_string} kommt 502")
-        time.sleep(2)
-        continue
-    el
-    """
     if r.status_code == 200:
         os.system("/opt/kite/kite-installer update")
         print(r.status_code)
@@ -48,6 +36,14 @@ while not i:
     else:
         print(f"{uhrzeit} Wir haben einen {r.status_code} Fehler")
         time.sleep(2)
+
+chek()
+
+while True:
+    try:
+        chek()
+    except:
+        time.sleep(10)
 
 listener.join()
 
